147d6db54116eef32bb204a69264a8044bc149bf
siiri <siiri.roschier@icloud.com>
2022-10-06 22:13:52 +0300

    homework

---
+++ nestedhw5_/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] A = {2, 2, 3, 4, 5};
+        System.out.println(uniques(A));
+    }
+    static boolean uniques (int [] numbers) {
+        int[] co = numbers;
+        boolean flaa = false;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int k = 0; k < numbers.length; k++) {
+                if (k != i && numbers [k] == co [i]) {
+                    flaa = true;
+                    break;
+                }
+            }
+        }
+        return !flaa;
+    }
+}
